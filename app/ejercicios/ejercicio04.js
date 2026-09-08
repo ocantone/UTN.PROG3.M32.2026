6855d32f089db238715e2bd6b52e3871a695dc91
Osvaldo Cantone <ocantone@gmail.com>
2026-09-08 10:39:48 -0300

    Actualizo ejercicio

--- app/ejercicios/ejercicio04.js
+++ app/ejercicios/ejercicio04.js
@@ -7,30 +7,23 @@
       let precioFinal = 100 - descuento; 
       console.log("Precio con descuento:", precioFinal); // ¿Qué imprime y por qué?
       */
-
+/*
       // Error 2: Mal uso de metodos de arreglo
       const valores = [10, 20, 30];
-      console.log(valores);
-    //  valores = [11, 21, 31];
-
-    valores[0] = 11;
-    valores[1] = 21;
-    valores[2] = 31;
-  
-      console.log(valores);
-
-      
-     /*
-      const dobles = valores.map(v => {
-          v * 2;
-      });
+      const dobles = valores.map(v =>  { v * 2} );
       console.log("Dobles:", dobles);
      
-      /*
+      */
+
       // Error 3: Mutación no deseada
       const configuracionInicial = { puerto: 8080, estado: 'OK' };
       const configuracionActual = configuracionInicial;
+      configuracionInicial.puerto = 8081;
+      
+      //configuracionInicial = { puerto: 8082, estado: 'OFF' };
+      
+      console.log(configuracionInicial.puerto);
       configuracionActual.puerto = 9090;
       
       console.log("¿El puerto inicial cambió?:", configuracionInicial.puerto);
-      */
+     
